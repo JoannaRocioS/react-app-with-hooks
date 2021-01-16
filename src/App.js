d67b037fdad116012aa31fef9ad7fa58ee1c5571
Joanna Sakugawa <joannarocios@gmail.com>
2021-01-16 15:54:05 -0300

    Creating React app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-content">
+        <p className="App-title">
+          Página de Gifs de Cartoons
+        </p>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
